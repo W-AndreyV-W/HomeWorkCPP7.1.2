590e456b1004598e6889c55caed2e75ec00789ea
Andrey V <Andreyxvv@hotmail.com>
2023-08-23 21:27:33 +0500

    first commit

---
+++ HomeWorkCPP7.1.2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "sql_select_query_builder.h"
+
+int main()
+{
+    SqlSelectQueryBuilder query_builder;
+    query_builder.AddColumns({ "name", "phone" });
+    query_builder.AddFrom("students");
+    query_builder.AddWhere({ { "id", "42" }, { "name", "John" } });
+
+    std::cout << query_builder.BuildQuery() << std::endl;
+}
